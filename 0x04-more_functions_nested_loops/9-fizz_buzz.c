3c5878366f3594cfeb14df1ce3153dce5193776e
joyfn <joynyumbaiza@gmail.com>
2022-09-15 16:32:52 -0700

    task9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,26 +16,26 @@ int main(void)
 	{
 		if ((num % 3 == 0) && (num % 5 == 0))
 		{
-			printf("FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if ((num % 3) == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if ((num % 5) == 0)
 		{
 			if (num != 100)
 			{
-				printf("Buzz");
+				printf("Buzz ");
 			}
 			else
 			{
-				printf("Buzz");
+				printf("Buzz ");
 			}
 		}
 		else
 		{
-			printf("%d", num);
+			printf("%d ", num);
 		}
 	}
 	printf("\n");
