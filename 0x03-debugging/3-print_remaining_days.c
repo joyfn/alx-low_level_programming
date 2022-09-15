4e46a1f8aa408eb5a99c9939d820f142ee8f5569
joyfn <joynyumbaiza@gmail.com>
2022-09-15 09:54:05 -0700

    task3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -15,7 +15,7 @@ void print_remaining_days(int month, int day, int year)
 	if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
 	{
 		if (month >= 3 && day >= 60)
-		{		
+		{
 			day++;
 		}
 
@@ -33,5 +33,5 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
-	}	
+	}
 }
