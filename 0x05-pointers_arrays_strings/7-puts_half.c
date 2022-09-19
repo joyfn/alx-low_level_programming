0fc128bce062184f7415301876c09e3cbf818b64
joyfn <joynyumbaiza@gmail.com>
2022-09-19 09:35:18 -0700

    t7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,15 +12,17 @@ void puts_half(char *str)
 	int x, y, i;
 
 	x = strlen(str);
-	if (x % 2 == 1)
-	{
-	   y = x / 2 + 1;
-	}
-	else
-	{
-		y = x / 2;
-	}
-	for (i = y; i < x; i++)
-	    _putchar(str[i]);
-	_putchar('\n');
+
+if (x % 2 == 1)
+{
+	y = x / 2 + 1;
+}
+else
+{
+	y = x / 2;
+}
+
+for (1 = y; i < x; i++)
+	_putchar(str[i]);
+_putchar('\n');
 }
