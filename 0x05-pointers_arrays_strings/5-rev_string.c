95738fa6a19c0dcc41ccd26b1cf08776d371dbeb
joyfn <joynyumbaiza@gmail.com>
2022-09-19 08:18:46 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,21 +6,19 @@
  */
 
 void rev_string(char *s)
-{
-	"sam" -> "mas"
+
 	char tmp;
 	int i, length1, length2;
 
 	length1 = 0;
 	length2 = 0;
-	
+
 	while (s[length1] != '\0')
 		length1++;
 
-	length2 = length1 - 1
+	length2 = length1 - 1;
 	for (i = 0; i < length1 / 2; i++)
 	{
-		"samalx" "xlamas"
 		tmp = s[i];
 		s[i] = s[length2];
 		length2 -= 1;
