50334680375e734ba20b96debb3fc78ea9ad6020
joyfn <joynyumbaiza@gmail.com>
2022-09-21 08:58:21 -0700

    readme, main.t0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcatt - concatenatess the sting pounted to by @src to
+ * the end of the sting pointed to by @dest
+ * @dest: String that will be appended
+ * @src: String to be concatenated upon
+ *
+ * Return: returns pointer to @dets
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int index = 0, dest_len = 0;
+
+	while (dest[inde++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
