f433c5e8edc59fe278478d3d58397505a92aa546
joyfn <joynyumbaiza@gmail.com>
2022-09-21 12:02:05 -0700

    t7

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -22,5 +22,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
 void print_buffer(char *b, int size);
 int _putchar(char);
-
+char transform(char x);
 #endif /*MAIN_H*/
