0a9e3fa1575a82b00753f3b59131dc8accf76edc
joyfn <joynyumbaiza@gmail.com>
2022-09-30 13:06:44 -0700

    t3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -> prints name
+ * @argc: argc parameter
+ * @argv: arrya of command
+ * Return: 0 for success
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+
+	printf("%d\n", result);
+	return (0);
+}
