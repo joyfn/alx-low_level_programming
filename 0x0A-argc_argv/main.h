c2ebb0ff72c6612379b7224692b5b57a3b42d96e
joyfn <joynyumbaiza@gmail.com>
2022-09-30 12:11:45 -0700

    main

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,3 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - entry point
+ * Description: 'the program's description'
+ * 'c' - output
+ * Return: 0
+ */
+
 int _putchar(char c);
 int check_num(char *str);
-int main(int argc, char *argv[]) 
+int main(int argc, char *argv[]);
+
+#endif
